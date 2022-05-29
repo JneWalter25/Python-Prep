1db16f7ab6ed90494f04ecd2bff7b53f8d5a5638
Jorge Walter <jorgewalterpro@gmail.com>
2022-05-28 19:15:14 -0500

    Homework 1 done

--- 02 - Variables y Tipos de Datos/Homework1.py
+++ 02 - Variables y Tipos de Datos/Homework1.py
@@ -1,3 +1,84 @@
 #1.
 x = 5
 print(x)
+
+#2. 
+print(type(8.5))
+
+#3.
+print(type(1))
+
+#4.
+y = "Walter"
+
+#5. 
+a = 3 +6j
+
+#6
+print(type(a))
+
+#7
+x = 3.1416
+
+#8 
+x = "True"
+y = True #No son lo mismo
+
+#9
+print(type(x),type(y))
+
+#10
+x = 5 + 2.5   
+
+#11
+x = (5+3j) + (4+2j)
+
+#12
+x = (5+3j) + 5
+
+#13
+print(2*2)
+
+#14
+print(2**8)
+
+#15
+print(27/4)
+
+#16
+print((27//4))
+
+#17
+print(27%4)
+
+#18
+print(4*6+3)
+
+#19
+print("hola "+"mundo")
+
+#20
+print("2"==2) #No son del mismo tipo
+
+#21
+x = "2"
+y = int(x)
+print(2==y)
+
+#22
+a = float('3.8') #Uso coma y no punto
+
+#23
+x = 3 
+x -= 2
+print(x)
+
+#24
+print(1<<2) #Returns x with the bits shifted to the left by y places (and new bits on the right-hand-side are zeros). This is the same as multiplying x by 2**y.
+
+#25
+#print(2+'2') No se pueden sumar de diferentes variables tipos usando +
+print(2+2)
+
+#26
+print("jorge tiene",2,"años")
